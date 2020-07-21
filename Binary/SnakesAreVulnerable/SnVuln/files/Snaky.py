01a4b2770c1e10ddf8120fab3c570bad65d8a023
shashank68 <shashankindiamanoj@gmail.com>
2020-07-22 02:33:17 +0530

    secured pyvulns

--- Binary/SnakesAreVulnerable/SnVuln/files/Snaky.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-sys.stdout.flush()
-print "\nGreetings from" + u"\U0001F40D" + ".\n\nCan you please give me your number"
-sys.stdout.flush()
-NUM = int(input())
-sys.stdout.flush()
-print "Hmm, I guess you people don't like snakes"
---
+++ Binary/SnakesAreVulnerable/pyvulns/Snaky.py
@@ -0,0 +1,9 @@
+#! /opt/python2/bin/python
+import sys
+
+sys.stdout.flush()
+print "\nCan you please give me your number"
+sys.stdout.flush()
+NUM = int(input())
+sys.stdout.flush()
+print "Hmm, I guess you people don't like snakes\nBye !hiss"
