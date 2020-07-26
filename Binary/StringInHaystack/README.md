ad104b3659176174327065ebcf0ddd29682a4aff
shashank68 <shashankindiamanoj@gmail.com>
2020-07-26 19:48:45 +0530

    haystck

--- Binary/StringInHaystack/README.md
+++ Binary/StringInHaystack/README.md
@@ -1,4 +1,4 @@
-flag: smz{5tr1ng5_4re_T4ngleD_0x01}
+flag: MAZ3{5tr1ng5_4re_T4ngleD_0x01}
 
 Hint: Some people have made it for you. Just use it.
 
--- Binary/StringInHaystack/strings.cpp
+++ Binary/StringInHaystack/strings.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 void pf();
-string fg1 = "smz{5tr1ng";
+string fg1 = "MAZ3{5tr1ng";
 int main() {
     puts("Get ready......");
     string pwd = "x1";
     pwd += "y2";
     pwd += "z3";
+    pwd += ".";
+    pwd += "^";
     string str;
     printf("Enter the password:  ");
     cin >> str;
@@ -22,7 +24,6 @@ int main() {
 string fg4 = "_0x01}";
 
 void pf() {
-    // smz{5tr1ng5_4re_T4ngleD_0x01}
     string fg2 = "5_4re_T4", fg3 = "ngleD";
     cout << fg1;
     cout << fg2;
